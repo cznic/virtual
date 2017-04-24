96dca8672d3d5c3964661a33355f549a2790740f
Steffen <steffen.butzer@outlook.com>
2017-04-24 17:53:17 +0200

    windows: fallback to 32-bits for readLong/writeLong OPcodes

--- cpu64_windows.go
+++ cpu64_windows.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 // +build amd64 amd64p32 arm64 mips64 mips64le mips64p32 mips64p32le ppc64 sparc64
-// +build !windows
 
 package virtual
 
@@ -13,7 +12,7 @@ import (
 )
 
 const (
-	longBits = 64
+	longBits = 32
 )
 
 func (c *cpu) push64(n, m int) {
@@ -27,7 +26,21 @@ func (c *cpu) pushC128(n, m int) {
 	c.ip++
 }
 
-func readLong(p uintptr) int64       { return *(*int64)(unsafe.Pointer(p)) }
-func readULong(p uintptr) uint64     { return *(*uint64)(unsafe.Pointer(p)) }
-func writeLong(p uintptr, v int64)   { *(*int64)(unsafe.Pointer(p)) = v }
-func writeULong(p uintptr, v uint64) { *(*uint64)(unsafe.Pointer(p)) = v }
+func readLong(p uintptr) int64   { return int64(*(*int32)(unsafe.Pointer(p))) }
+func readULong(p uintptr) uint64 { return uint64(*(*uint32)(unsafe.Pointer(p))) }
+
+func writeLong(p uintptr, v int64) {
+	if v < math.MinInt32 || v > math.MaxInt32 {
+		panic("size_t overflow")
+	}
+
+	*(*int32)(unsafe.Pointer(p)) = int32(v)
+}
+
+func writeULong(p uintptr, v uint64) {
+	if v > math.MaxUint32 {
+		panic("size_t overflow")
+	}
+
+	*(*uint32)(unsafe.Pointer(p)) = uint32(v)
+}
